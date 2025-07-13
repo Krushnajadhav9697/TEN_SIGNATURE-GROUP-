9474cd23346ab064a1ec1a9c6e63d1ee3fa9b4ee
krushnajadhav9697 <krushnajadhav9697@gmail.com>
2025-07-13 19:52:52 +0530

    aded mail and pass for otp sending

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -173,8 +173,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'negisamay6@gmail.com'
-EMAIL_HOST_PASSWORD = 'izyq xkyj kszq eiep'
+
+
+EMAIL_HOST_USER = 'krushh45@gmail.com'
+EMAIL_HOST_PASSWORD = 'iavq jftk aacx dnda'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
